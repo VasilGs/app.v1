6cd34c317961a5aba928e8bcbbbc6e66a1c9c5e4
VasilGs <v.dostinovv@gmail.com>
2025-08-24 14:35:56 +0300

    Updated App.tsx

--- src/features/seeker/App.tsx
+++ src/features/seeker/App.tsx
@@ -1,24 +1,15 @@
-// src/features/seeker/App.tsx
-import * as React from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
-// import your seeker pages/components here
-// e.g., Profile, Swipe, Saved, Settings, etc.
-
-function SwipePage() {
-  return <div className="p-6">TODO: Swipe deck here</div>;
-}
-
-function ProfilePage() {
-  return <div className="p-6">TODO: Seeker profile here</div>;
-}
+import { Routes, Route, Navigate } from "react-router-dom";
+import SwipeDeck from "./SwipeDeck"; // create/use your swipe deck component
+import UserProfileView from "../../components/UserProfileView"; 
+import SavedJobsModal from "../../components/SavedJobsModal"; 
 
 export default function SeekerApp() {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="swipe" />} />
-      <Route path="swipe" element={<SwipePage />} />
-      <Route path="profile" element={<ProfilePage />} />
-      {/* add more seeker routes as needed */}
+      <Route path="swipe" element={<SwipeDeck />} />
+      <Route path="profile" element={<UserProfileView />} />
+      <Route path="saved" element={<SavedJobsModal />} />
     </Routes>
   );
 }
